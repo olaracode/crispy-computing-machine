97ed0f8451fdb77e5cb481f8bf3b3ff962faf38e
Wes Watson <wesrwats@gmail.com>
2025-02-26 08:11:34 -0600

    Add Base

--- version/vite.config.js
+++ version/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
   },
-  base: "https://vite.dev/Final-Project/",
+  base: "/Final-Project/",
 });
