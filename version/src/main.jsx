517a79d7dd252203d2584e153f12100c6aae461d
Wes Watson <wesrwats@gmail.com>
2025-02-26 08:31:56 -0600

    Update main.jsx

--- version/src/main.jsx
+++ version/src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 //import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./components/App/App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
